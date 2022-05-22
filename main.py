6e426675ae25bef95d46ec0b31fc558d0c607453
guitarkeegan <keegananglim@gmail.com>
2022-05-22 07:31:58 -0700

    save quiz question to external text file for further cleaning

--- main.py
+++ main.py
@@ -2,9 +2,23 @@ import requests
 from bs4 import BeautifulSoup
 import requests
 from pprint import pprint
+import re
 
-response = requests.get("https://codeexercise.com/50-top-javascript-multiple-choice-questions-and-answers/")
-html = response.text
-soup = BeautifulSoup(html, 'html.parser')
-ordered_lists = soup.select("ol")
-pprint(ordered_lists)
+# make a get request to the server
+# response = requests.get("https://codeexercise.com/50-top-javascript-multiple-choice-questions-and-answers/")
+
+# extract the text to create a readable html document
+# html = response.text
+
+# use the html parser to create the soup object
+# soup = BeautifulSoup(html, 'html.parser')
+
+# grab the desired tags
+# ordered_lists = soup.select("ol li p")
+
+# save the desired text to a file so that I don't need to continually make the get requests
+# with open("quiz_questions.txt", "w") as questions_raw:
+#     questions_raw.write(str(ordered_lists))
+
+
+
