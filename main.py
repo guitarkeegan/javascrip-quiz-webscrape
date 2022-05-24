e006424eba951d88e91cfb46d17e57ad53d07b58
guitarkeegan <keegananglim@gmail.com>
2022-05-23 23:39:56 -0700

    made as object-like as possible

--- main.py
+++ main.py
@@ -27,12 +27,19 @@ with open("quiz_questions.txt", "r") as questions_raw:
 rm_p_tags = qw.replace("<p>", "{")
 rm_p_close_tags = rm_p_tags.replace("</p>", "")
 rm_br_tags = rm_p_close_tags.replace(r"<br/>", "")
-rm_newlines = rm_br_tags.replace("\n", " \n")
+rm_newlines = rm_br_tags.replace("\n", "', \n")
 a_choice = rm_newlines.replace("A.", "A:")
 b_choice = a_choice.replace("B.", "B:")
 c_choice = b_choice.replace("C.", "C:")
 d_choice = c_choice.replace("D.", "D:")
-pprint(d_choice)
+rm_commas = re.sub(r",\s{", "}{", d_choice)
+finish_object = rm_commas.replace(" \n", ",")
+beg_quotes = rm_commas.replace(": ", ": '")
+q_object = beg_quotes.replace("{", "{question: '")
+sub_final = q_object.replace(" \n", "")
+final = sub_final.replace("}", "'}, ")
+
+
 
 # rm_fbracket = rm_newlines.replace("[", "")
 # rm_bbracket = rm_fbracket.replace("]", "")
