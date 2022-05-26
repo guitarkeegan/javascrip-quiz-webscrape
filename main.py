c5f179b100174e931471af71cdba9b5d59e2fe41
guitarkeegan <keegananglim@gmail.com>
2022-05-26 09:16:33 -0700

    uncomment cleaning of text

--- main.py
+++ main.py
@@ -1,24 +1,23 @@
-import requests
 from bs4 import BeautifulSoup
 import requests
 from pprint import pprint
 import re
 
-# make a get request to the server
-# response = requests.get("https://codeexercise.com/50-top-javascript-multiple-choice-questions-and-answers/")
+# make a GET request to the server
+response = requests.get("https://codeexercise.com/50-top-javascript-multiple-choice-questions-and-answers/")
 
 # extract the text to create a readable html document
-# html = response.text
+html = response.text
 
 # use the html parser to create the soup object
-# soup = BeautifulSoup(html, 'html.parser')
+soup = BeautifulSoup(html, 'html.parser')
 
 # grab the desired tags
-# ordered_lists = soup.select("ol li p")
+ordered_lists = soup.select("ol li p")
 
 # save the desired text to a file so that I don't need to continually make the get requests
-# with open("quiz_questions.txt", "w") as questions_raw:
-#     questions_raw.write(str(ordered_lists))
+with open("quiz_questions.txt", "w") as questions_raw:
+    questions_raw.write(str(ordered_lists))
 
 with open("quiz_questions.txt", "r") as questions_raw:
     qw = questions_raw.read()
@@ -38,13 +37,10 @@ beg_quotes = rm_commas.replace(": ", ": '")
 q_object = beg_quotes.replace("{", "{question: '")
 sub_final = q_object.replace(" \n", "")
 final = sub_final.replace("}", "'}, ")
-print(final)
+pprint(final)
 with open("formatted_question.txt", "w") as fq:
     fq.write(final)
 
-
-# rm_fbracket = rm_newlines.replace("[", "")
-# rm_bbracket = rm_fbracket.replace("]", "")
 
 
 
